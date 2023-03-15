98fb9065ed7dab5bf1924f6b61713ac1256f7ce1
Michael Auderer <michael@stackmind.com>
2023-03-15 14:56:01 -0400

    stop clicking sort

--- js/rsm_activate-filters.js
+++ js/rsm_activate-filters.js
@@ -93,49 +93,39 @@ params.forEach((value, key) => {
    }
 })
 
-if (selectedFilters.length === 1) {
-   document.querySelectorAll('.sort-menu-wrapper > a > div').forEach(div => {
-      if (div.textContent === selectedFilters[0]) {
-         div.parentElement.click()
-         console.log(div.parentElement)
-         return
-      }
-   })
-}
-
 selectedFilters.forEach(filter => {
    clickFilter(filter)
 })
 
 // wait for javascript to finish loading posts into DOM
-setTimeout(function() {
-  const post_list = Array.from(document.querySelectorAll('#post_list .w-dyn-item'))
-     .filter(x => x.style.display !== 'none')
-     .map(x => x.querySelector('a'))
-  const current_post = document.querySelector('.w--current');
-  const current_post_idx = [].indexOf.call(post_list, current_post);
-  const next_post = post_list[current_post_idx + 1];
+setTimeout(function () {
+   const post_list = Array.from(document.querySelectorAll('#post_list .w-dyn-item'))
+      .filter(x => x.style.display !== 'none')
+      .map(x => x.querySelector('a'))
+   const current_post = document.querySelector('.w--current');
+   const current_post_idx = [].indexOf.call(post_list, current_post);
+   const next_post = post_list[current_post_idx + 1];
 
-  let next_href, next_title;
+   let next_href, next_title;
 
-  if (next_post) {
-    next_href = next_post.getAttribute('href');
-    next_title = next_post.textContent;
-  } else {
-    // last post in list
-    const first_post = post_list[0];
-    next_href = first_post.getAttribute('href');
-    next_title = first_post.textContent;
-  }
+   if (next_post) {
+      next_href = next_post.getAttribute('href');
+      next_title = next_post.textContent;
+   } else {
+      // last post in list
+      const first_post = post_list[0];
+      next_href = first_post.getAttribute('href');
+      next_title = first_post.textContent;
+   }
 
-  if (params.toString()) {
-     next_href = next_href + '?' + params.toString();
-  }
+   if (params.toString()) {
+      next_href = next_href + '?' + params.toString();
+   }
 
-  //apply hrefs to next / previous buttons
-  document.querySelector('#next_button').setAttribute('href', next_href);
-  document.querySelector('#next_title').innerText = next_title;
+   //apply hrefs to next / previous buttons
+   document.querySelector('#next_button').setAttribute('href', next_href);
+   document.querySelector('#next_title').innerText = next_title;
 
-  // hide post list for accessibility
-  document.getElementById('post_list').style.display = 'none';
+   // hide post list for accessibility
+   document.getElementById('post_list').style.display = 'none';
 }, 5000);
